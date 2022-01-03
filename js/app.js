567c5826316666f503889576f06551fa7423dcce
lucashonma <lucashonma@github.com>
2022-01-03 11:54:35 -0300

    Terms Popup and links for social media icons

--- js/app.js
+++ js/app.js
@@ -21,11 +21,13 @@ const highlightMenu = () => {
   // adds 'highlight' class to my menu items
   if (window.innerWidth > 960 && scrollPos < 600) {
     homeMenu.classList.add('highlight');
+    projectsMenu.classList.remove('highlight');
     aboutMenu.classList.remove('highlight');
     hideMobileMenu;
     return;
   } else if (window.innerWidth > 960 && scrollPos < 1400) {
     projectsMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
     aboutMenu.classList.remove('highlight');
     hideMobileMenu;
     return;
@@ -58,25 +60,20 @@ const hideMobileMenu = () => {
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 
-var modal = document.getElementById("modal");
+var modal = document.getElementById("termsConditions");
 
-// Get the button that opens the modal
-var btn = document.getElementById("terms");
+var btn = document.getElementById("termsModal");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
